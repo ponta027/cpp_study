b71a2ccc220900c7a76db1a8bad851b19659c68d
ponta027 <ponta027@gmail.com>
2021-01-31 18:20:26 +0900

    add function object sample

--- functional/src/functional_sample.cpp
+++ functional/src/functional_sample.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
 
+struct doubler {
+  int operator()(int x) const { return x * 2; }
+};
 
-int main(int argc, char* argv[]) { return 0; }
+int main(int argc, char* argv[]) {
+  auto x = 10;
+  doubler d;
+  std::cout << d(x) << std::endl;
+
+  return 0;
+}
